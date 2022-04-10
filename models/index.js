a2a0c868e58b009f7a0639691810b4f50708357c
jstable1 <jessica.stabler@gmail.com>
2022-04-09 22:42:26 -0600

    created models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
+  through: ProductTag,
+  as: 'tagged_product',
+  
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+});
 
 module.exports = {
   Product,
